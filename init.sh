60d8e19f56c48526f4c5e566285002291103f7b7
ta-yuhyeon <codesaturday@gmail.com>
2025-12-14 01:09:50 +0100

    first commit

---
+++ init.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "vagrant ALL=(ALL) NOPASSWD: ALL" > test.txt
+
+mkdir /home/vagrant/.ssh/
+chmod 0700 /home/vagrant/.ssh/
+touch /home/vagrant/.ssh/authorized_keys
+chown vagrant:vagrant /home/vagrant/ssh
+
+chmod 0600 / home/vagrant/.ssh/authorized_keys
+echo "vagrant ALL=(ALL) NOPASSWD:ALL" | tee /etc/sudoers.d/vagrant
+chmod 448 /etc/sudoers.d/vagrant
+chown vagrant:vagrant /home/vagrant/.ssh/authorized_keys
+
+wget https://raw.githubusercontent.com/hashicorp/vagrant/refs/heads/main/keys/vagrant.pub -0 /home/vagrant/.ssh/authorized_keys
+
+systemctl restart sshd
