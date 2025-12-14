acf70970b7dba4fd5d6a6c34dbf605bc0870ea3d
lo-yuta <77937738+lo-yuta@users.noreply.github.com>
2025-12-14 01:18:37 +0100

    Refactor init script for improved setup process

--- init.sh
+++ init.sh
@@ -1,17 +1,19 @@
-#!/bin/sh
+#!/bin/bash
+set -e
 
-apt install sudo -y
+apt-get update -y
+apt-get install -y sudo wget
 
-mkdir /home/vagrant/.ssh/
-chmod 0700 /home/vagrant/.ssh/
-touch /home/vagrant/.ssh/authorized_keys
-chown vagrant:vagrant /home/vagrant/ssh
+mkdir -p /home/vagrant/.ssh
+chmod 700 /home/vagrant/.ssh
 
-chmod 0600 / home/vagrant/.ssh/authorized_keys
-echo "vagrant ALL=(ALL) NOPASSWD:ALL" | tee /etc/sudoers.d/vagrant
-chmod 448 /etc/sudoers.d/vagrant
-chown vagrant:vagrant /home/vagrant/.ssh/authorized_keys
+wget -qO /home/vagrant/.ssh/authorized_keys \
+  https://raw.githubusercontent.com/hashicorp/vagrant/master/keys/vagrant.pub
 
-wget https://raw.githubusercontent.com/hashicorp/vagrant/refs/heads/main/keys/vagrant.pub -0 /home/vagrant/.ssh/authorized_keys
+chmod 600 /home/vagrant/.ssh/authorized_keys
+chown -R vagrant:vagrant /home/vagrant/.ssh
 
-systemctl restart sshd
+echo "vagrant ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/vagrant
+chmod 440 /etc/sudoers.d/vagrant
+
+systemctl restart ssh || systemctl restart sshd
